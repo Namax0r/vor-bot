188c0a68b653adf9a9bc65706573fc65e6ad1f92
Namax0r <patrykiakos@protonmail.com>
2016-11-19 22:02:34 +0100

    Add new function, general code refactoring

--- vor_bot.py
+++ vor_bot.py
@@ -1,11 +1,15 @@
 import discord
 from discord.ext import commands
 import random
+# randint is needed to generate random numbers.
+from random import randint
 import asyncio
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-There are a number of utility commands being showcased here.'''
-bot = commands.Bot(command_prefix='?', description=description)
+
+# set description
+description = '''Discord vor-bot based on https://github.com/Rapptz/discord.py'''
+# create bot
+
+bot = commands.Bot(command_prefix='!', description=description)
 
 @bot.event
 @asyncio.coroutine
@@ -15,57 +19,58 @@ def on_ready():
     print(bot.user.id)
     print('------')
 
+commands_help = {"add": "Adds two numbers together", "joined": "Displays information about user"}
+
+@bot.command()
+@asyncio.coroutine
+def commands():
+    #display all the commands available
+    for key, value in commands_help.items():
+        yield from bot.say('{0}{1} = {2}{3}'.format('```', key, value, '```'))
+
+# Adds two numbers together.
 @bot.command()
 @asyncio.coroutine
 def add(left : int, right : int):
-    """Adds two numbers together."""
     yield from bot.say(left + right)
 
+# Rolls a basic 1-6 dice.
 @bot.command()
 @asyncio.coroutine
-def roll(dice : str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        yield from bot.say('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+def roll():
+    result = randint(1,6)
     yield from bot.say(result)
 
-@bot.command(description='For when you wanna settle the score some other way')
+# Chooses random item from given list of choices
+@bot.command()
 @asyncio.coroutine
 def choose(*choices : str):
-    """Chooses between multiple choices."""
     yield from bot.say(random.choice(choices))
 
+# Repeats a message given numbers of times
 @bot.command()
 @asyncio.coroutine
 def repeat(times : int, content='repeating...'):
-    """Repeats a message multiple times."""
     for i in range(times):
         yield from bot.say(content)
 
+# Displays information when a member joined
 @bot.command()
 @asyncio.coroutine
 def joined(member : discord.Member):
-    """Says when a member joined."""
     yield from bot.say('{0.name} joined in {0.joined_at}'.format(member))
 
+# Says if user is cool
 @bot.group(pass_context=True)
 @asyncio.coroutine
 def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
     if ctx.invoked_subcommand is None:
         yield from bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
 
-@cool.command(name='bot')
+'''@cool.command(name='bot')
 @asyncio.coroutine
 def _bot():
     """Is the bot cool?"""
-    yield from bot.say('Yes, the bot is cool.')
+    yield from bot.say('Yes, the bot is cool.')'''
 
 bot.run('token')
